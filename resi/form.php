823669f6a812186946a28855af0031156d9d51ac
zxzcafzvsx <31308588+zxzcafzvsx@users.noreply.github.com>
2018-01-01 18:10:38 +0700

    Add files via upload

---
+++ resi/form.php
@@ -0,0 +1,5 @@
+<form method="get" action="./jne.php">
+<input type="text" name="awb" placeholder="awb"/>
+<input type="text" name="jasa" placeholder="jasa"/>
+<input type="submit" value="Submit"/>
+</form>
---
+++ resi/jne.php
@@ -0,0 +1,16 @@
+<?php  
+$awb = $_GET['awb'];
+$jasa = $_GET['jasa'];
+$sumber = 'http://wahidganteng.ga/process/api/b967d83eed40cf9e17958b1dc85b1db7/cek-resi?jasa='.$jasa.'&resi='.$awb;
+$konten = file_get_contents($sumber);
+$data = json_decode($konten, true);
+
+ echo "<h3>Cek Resi JNE, fitur ini masih BETA dan masih dalam tahap pengembangan</h3>";
+ echo "<br/>";
+?>
+<?php   
+for($i=0; $i < count($data['manifest']); $i++) {
+print $data['manifest'][$i]['manifest_date']."</br>";
+print $data['manifest'][$i]['manifest_desc']."</br></br>";
+}
+?>
