483a932c510982f83b69b980d4c4284353b06cdd
zxzcafzvsx <31308588+zxzcafzvsx@users.noreply.github.com>
2018-01-01 18:53:23 +0700

    Update jne.php

--- resi/jne.php
+++ resi/jne.php
@@ -5,7 +5,7 @@ $sumber = 'http://wahidganteng.ga/process/api/b967d83eed40cf9e17958b1dc85b1db7/c
 $konten = file_get_contents($sumber);
 $data = json_decode($konten, true);
 
- echo "Cek Resi JNE, fitur ini masih BETA dan masih dalam tahap pengembangan";
+ echo "Cek Resi $jasa, fitur ini masih BETA dan masih dalam tahap pengembangan";
  echo "</br></br></br>";
 ?>
 <?php   
